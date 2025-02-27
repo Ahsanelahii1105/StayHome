f9e2ae972591b64f5317e86a7df5a13925f383d1
Ahsanelahii1105 <ahsan2308e@aptechgdn.net>
2025-02-27 17:52:29 +0500

    first

---
+++ test/Controllers/AdminController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace test.Controllers
+{
+    public class AdminController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Agent()
+        {
+            return View();
+        }
+        public IActionResult Saler()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ test/Controllers/WebController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace test.Controllers
+{
+    public class WebController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        public IActionResult Contactus()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            return View();
+        }
+        public IActionResult Listning()
+        {
+            return View();
+        }
+        public IActionResult service()
+        {
+            return View();
+        }
+        public IActionResult Blogs()
+        {
+            return View();
+        }
+        public IActionResult Agent()
+        {
+            return View();
+        }
+    }
+}
